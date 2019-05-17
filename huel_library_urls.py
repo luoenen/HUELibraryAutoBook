dc337b63fd4045b2b39a234e41f5b7a7b6ab8d41
Tech申申 <42631387+luoenen@users.noreply.github.com>
2019-05-17 21:03:23 +0800

    Update huel_library_urls.py

--- huel_library_urls.py
+++ huel_library_urls.py
@@ -6,6 +6,7 @@
 index_url = 'http://wechat.laixuanzuo.com/index.php/reserve/index.html?f=wechat'
 
 # 明日预定      https://wechat.laixuanzuo.com/index.php/reserve/layoutApi/action=prereserve_event&libid=10065
+              https://wechat.laixuanzuo.com/index.php/prereserve/save/libid=10065&sTKmM3nQBnQEx=7,14&yzm=2191
 booking_url = 'http://wechat.laixuanzuo.com/index.php/prereserve/index.html'
 
 # 预定
